9fcc700b6007d45f475d2750f7ac76066af02e44
Ubuntu <ubuntu@ip-172-31-54-80.ap-northeast-2.compute.internal>
2022-01-10 19:56:45 +0000

    Deploy application by nginx.

--- app.py
+++ app.py
@@ -4,9 +4,17 @@ from api import api
 from flask.json import JSONEncoder
 from sqlalchemy import create_engine, text
 from global_things.functions import slack_error_notification
+import logging
 
 app = Flask(__name__)
 CORS(app)
+
+APP_ROOT="/home/ubuntu/circlinMembersApi_python/circlinMembersApi_flask"
+logging.basicConfig(filename=f'{APP_ROOT}/execution_log.log', filemode='a+', format=' [%(filename)s:%(lineno)s:%(funcName)s()]- %(asctime)s - %(levelname)s - %(message)s', level=logging.INFO)
+gunicorn_logger = logging.getLogger('gunicorn.error')
+app.logger.handlers = gunicorn_logger.handlers
+app.logger.setLevel(gunicorn_logger.level)
+
 app.config.from_pyfile('./config/database.py')
 app.register_blueprint(api, url_prefix="/api")
 try:
@@ -17,7 +25,7 @@ except Exception as e:
     print(e)
 
 
-@app.route('/')
+@app.route('/testing')
 def hello_world():
     return 'Hello World!'
 
