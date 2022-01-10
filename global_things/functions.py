bd18771e86ed7edae7dc62a50569b2fbe856be73
circlindev-caleb <caleb@circlin.co.kr>
2022-01-11 00:04:06 +0900

    Modified indentations.

--- global_things/functions.py
+++ global_things/functions.py
@@ -7,10 +7,10 @@ def slack_error_notification(user_ip: str='', user_id: str='', nickname: str='',
     json.dumps({
       "channel": "#circlin-members-log",
       "username": "써클인 멤버스 - python",
-      "text": f"써클인 멤버스(python)에서 오류가 발생했습니다. \n \
-*사용자 IP*: `{user_ip}` \n \
-*닉네임 (ID)*: `{nickname}({user_id})`\n \
-*API URL*: `{api}` \n \
+      "text": f"*써클인 멤버스(python)에서 오류가 발생했습니다.* \n \
+사용자 IP: `{user_ip}` \n \
+닉네임 (ID): `{nickname}({user_id})`\n \
+API URL: `{api}` \n \
 ```{error_log}```",
       "icon_url": "https://www.circlin.co.kr/new/assets/favicon/apple-icon-180x180.png"
     }
