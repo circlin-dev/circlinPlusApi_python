95133a18174e8d68e4b9df070c6a0b95aa4245fd
circlindev-caleb <caleb@circlin.co.kr>
2022-01-15 00:42:57 +0900

    Added date input form to bodylab_form to get the target dates of feedback.

--- api/bodylab.py
+++ api/bodylab.py
@@ -1,4 +1,4 @@
-from global_things.functions import slack_error_notification, analyze_image, login_to_db
+from global_things.functions import slack_error_notification, analyze_image, get_date_range_from_week, login_to_db
 from global_things.constants import ATTRACTIVENESS_SCORE_CRITERIA
 from . import api
 from flask import request
@@ -8,6 +8,7 @@ import json
 def add_weekly_data():
   ip = request.remote_addr
   endpoint = '/api/bodylab/add'
+  period = request.form.get('period')  # Value format: yyyy-Www(Week 01, 2017 ==> "2017-W01")
   user_id = request.form.get('user_id')
   height = request.form.get('height')
   weight = request.form.get('weight')
@@ -17,13 +18,24 @@ def add_weekly_data():
   body_image = request.form.get('body_image')
 
   if request.method == 'POST':
-    if not(user_id and height and weight and bmi and muscle_mass and fat_mass and body_image):
+    if not(period and user_id and height and weight and bmi and muscle_mass and fat_mass and body_image):
       result = {
         'result': False,
         'error': f'Missing data in request.',
-        'values': [user_id, height, weight, bmi, muscle_mass, fat_mass, body_image]
+        'values': {
+          'period': period,
+          'user_id': user_id,
+          'height': height,
+          'weight': weight,
+          'bmi': bmi,
+          'muscle_mass': muscle_mass,
+          'fat_mass': fat_mass,
+          'body_image': body_image
+        }
       }
       return json.dumps(result, ensure_ascii=False), 400
+
+    year, week_number_of_year, firstdate_of_week, lastdate_of_week = get_date_range_from_week(period.split('-W')[0], period.split('-W')[1])
 
     try:
       connection = login_to_db()
@@ -39,14 +51,22 @@ def add_weekly_data():
     cursor = connection.cursor()
     query = f'''
                 INSERT INTO bodylab(
-                    user_id, 
-                    height, 
-                    weight, 
-                    bmi, 
-                    muscle_mass, 
+                    user_id,
+                    year,
+                    week_number_of_year,
+                    firstdate_of_week,
+                    lastdate_of_week,
+                    height,
+                    weight,
+                    bmi,
+                    muscle_mass,
                     fat_mass)
                 VALUES (
                     {user_id},
+                    {year},
+                    {week_number_of_year},
+                    {firstdate_of_week},
+                    {lastdate_of_week},         
                     {height},
                     {weight},
                     {bmi},
@@ -75,20 +95,18 @@ def add_weekly_data():
             user_id={user_id}
         ORDER BY id DESC
             LIMIT 1'''
-    try:
-      cursor.execute(query)
-      latest_bodylab_id_tuple = cursor.fetchall()
+
+    cursor.execute(query)
+    latest_bodylab_id_tuple = cursor.fetchall()
+    if len(latest_bodylab_id_tuple) == 0 or latest_bodylab_id_tuple == ():
+      result = {
+        'result': False,
+        'error': f'Cannot find requested bodylab data of user(id: {user_id})(bodylab)'
+      }
+      slack_error_notification(user_ip=ip, user_id=user_id, api=endpoint, error_log=result['error'], query=query)
+      return json.dumps(result, ensure_ascii=False), 400
+    else:
       latest_bodylab_id = latest_bodylab_id_tuple[0][0]
-    except Exception as e:
-      connection.close()
-      error = str(e)
-      if len(latest_bodylab_id_tuple) == 0 or latest_bodylab_id_tuple == ():
-        result = {
-          'result': False,
-          'error': f'Cannot find requested bodylab data of user(id: {user_id})(bodylab): {error}'
-        }
-        slack_error_notification(user_ip=ip, user_id=user_id, api=endpoint, error_log=result['error'], query=query)
-        return json.dumps(result, ensure_ascii=False), 400
 
     # Analyze user's image and store the result.
     image_analysis_result = analyze_image(user_id, body_image)
@@ -222,3 +240,167 @@ def add_weekly_data():
     }
     ##########################################################
   '''
+
+# @api.route('/bodylab/weekly/<user_id>/<period>', methods=['GET'])
+# def read_weekly_score(user_id, period):
+#   endpoint = '/api/bodylab/weekly/<user_id>'
+#   ip = request.remote_addr
+#   user_id = request.args.get(user_id)
+#   period = request.args.get(period)
+#
+#   try:
+#     connection = login_to_db()
+#   except Exception as e:
+#     error = str(e)
+#     result = {
+#       'result': False,
+#       'error': f'Server Error while connecting to DB:{error}'
+#     }
+#     slack_error_notification(user_ip=ip, user_id=user_id, api=endpoint, error_log=error)
+#     return json.dumps(result, ensure_ascii=False), 500
+#
+#   cursor = connection.cursor()
+#   query = f'''
+#     SELECT * FROM bodylab
+#             (user_id,
+#             height,
+#             weight,
+#             bmi,
+#             muscle_mass,
+#             fat_mass)
+#       WHERE
+#             user_id = {user_id}
+#         AND
+#             created_at = {period}'''
+#   try:
+#     cursor.execute(query)
+#   except Exception as e:
+#     connection.close()
+#     error = str(e)
+#     result = {
+#       'result': False,
+#       'error': f'Server Error while executing SELECT query: {error}'
+#     }
+#     slack_error_notification(user_ip=ip, user_id=user_id, api=endpoint, error_log=error, query=query)
+#     return json.dumps(result), 500
+#
+#   return ''
+#
+# def calculate_body_score(score_type: str, my_bmi: float, my_fat: float, my_muscle: float):
+#   # 신체 점수
+#   """
+#   반환값 정의
+#   :
+#   :param score_type:
+#   :param my_bmi:
+#   :param my_fat:
+#   :param my_muscle:
+#   :return:
+#   """
+#
+#   if score_type == 'bmi':
+#     pass
+#   elif score_type == 'fat':
+#     pass
+#   elif score_type == 'muscle':
+#     pass
+#
+# def get_comment():
+#   # 수치에 따른 상단 한줄 코멘트
+#   return ''
+#
+# def get_my_amount():
+#   # 나의 BMI, 체지방, 근육량 DB에서 조회하기
+#   return ''
+#
+# def get_preferred_amount():
+#   return ''
+#
+# def calculate_gap():
+#   # 권장 수치와 내 수치의 차이
+#
+#   # 이성 선호, 동성 선호와 내 수치의 차이
+#   return ''
+#
+# def get_recommended_amount(target: str = '', height: float = 0.0, age: int = 0, weight: float = 0.0, sex: str = ''):
+#   # 권장 수치
+#   if target == 'bmi':
+#     recommended_bmi = '18.5 ~ 22.9'
+#   elif target == 'fat':
+#     if sex == 'male':
+#       if age < 18:
+#         percent = 9.4
+#       elif age < 21:
+#         percent = 10.5
+#       elif age < 26:
+#         percent = 11.6
+#       elif age < 31:
+#         percent = (12.7 + 14.6) / 2
+#       elif age < 36:
+#         percent = (13.7 + 15.7) / 2
+#       elif age < 41:
+#         percent = 16.8
+#       elif age < 46:
+#         percent = 17.8
+#       elif age < 51:
+#         percent = (18.9 + 20.7) / 2
+#       elif age < 56:
+#         percent = (20.0 + 21.8) / 2
+#       else:
+#         percent = 22.8
+#     else:
+#       if age < 18:
+#         percent = 19.7
+#       elif age < 21:
+#         percent = (19.7 + 21.5) / 2
+#       elif age < 26:
+#         percent = 22.1
+#       elif age < 31:
+#         percent = 22.7
+#       elif age < 36:
+#         percent = (23.4 + 25.1) / 2
+#       elif age < 41:
+#         percent = (24.0 + 25.7) / 2
+#       elif age < 46:
+#         percent = (24.6 + 26.3) / 2
+#       elif age < 51:
+#         percent = 26.9
+#       elif age < 56:
+#         percent = 27.6
+#       else:
+#         percent = (28.2 + 29.8) / 2
+#     recommended_fat_mass = (weight * percent) / 100
+#   elif target == 'muscle':
+#     if sex == 'male':
+#       recommended_muscle_mass = weight * 0.4
+#     else:
+#       recommended_muscle_mass = weight * 0.34
+#
+#   return recommended_bmi, recommended_fat_mass, recommended_muscle_mass
+#
+# def calculate_attractiveness_score(score_type: str):
+#   if score_type == 'bmi':
+#     pass
+#   elif score_type == 'fat':
+#     pass
+#   elif score_type == 'muscle':
+#     pass
+#
+# def history_data():
+#   # 최근 1주일의 날짜 & 일자별 내 점수
+#   return ''
+#
+# def get_image_analysis(user_id: int):
+#   # (7) 사진 분석값
+#   return ''
+#
+# def calculate_total_attractiveness_score():
+#   # (3) 매력 점수: 총점, (전체 동성 유저 중)순위
+#   # total_score
+#   # ranking
+#   return ''
+#
+# def calculate_total_body_score():
+#   # (2) 신체 점수: 총점, (전체 동성 유저 중)순위
+#   return ''
+# endregion
--- global_things/functions.py
+++ global_things/functions.py
@@ -1,5 +1,6 @@
 from global_things.constants import IMAGE_ANALYSYS_SERVER, SLACK_NOTIFICATION_WEBHOOK
 from config.database import DB_CONFIG
+import datetime
 import json
 import requests
 import pymysql
@@ -7,134 +8,201 @@ import pymysql
 
 # region Connection to database.
 def login_to_db():
-    conn = pymysql.connect(
-        user=DB_CONFIG['user'],
-        passwd=DB_CONFIG['password'],
-        host=DB_CONFIG['host'],
-        db=DB_CONFIG['database'],
-        charset=DB_CONFIG['charset'])
+  conn = pymysql.connect(
+    user=DB_CONFIG['user'],
+    passwd=DB_CONFIG['password'],
+    host=DB_CONFIG['host'],
+    db=DB_CONFIG['database'],
+    charset=DB_CONFIG['charset'])
 
-    return conn
-
+  return conn
 
 # endregion
+
 
 # region Slack error notification
 def slack_error_notification(user_ip: str = '', user_id: str = '', nickname: str = '', api: str = '',
                              error_log: str = '', query: str = ''):
-    if user_ip == '' or user_id == '':
-        user_ip = "Server error"
-        user_id = "Server error"
+  if user_ip == '' or user_id == '':
+    user_ip = "Server error"
+    user_id = "Server error"
 
-    send_notification_request = requests.post(
-        SLACK_NOTIFICATION_WEBHOOK,
-        json.dumps({
-            "channel": "#circlin-members-log",
-            "username": "써클인 멤버스 - python",
-            "text": f"*써클인 멤버스(python)에서 오류가 발생했습니다.* \n \
+  send_notification_request = requests.post(
+    SLACK_NOTIFICATION_WEBHOOK,
+    json.dumps({
+      "channel": "#circlin-members-log",
+      "username": "써클인 멤버스 - python",
+      "text": f"*써클인 멤버스(python)에서 오류가 발생했습니다.* \n \
 사용자 IP: `{user_ip}` \n \
 닉네임 (ID): `{nickname}({user_id})`\n \
 API URL: `{api}` \n \
 ```{error_log} \n \
 {query}```",
-            "icon_url": "https://www.circlin.co.kr/new/assets/favicon/apple-icon-180x180.png"
-        }, ensure_ascii=False)
-    )
+      "icon_url": "https://www.circlin.co.kr/new/assets/favicon/apple-icon-180x180.png"
+    }, ensure_ascii=False)
+  )
 
-    return send_notification_request
-
-
+  return send_notification_request
 # endregion
+
 
 # region Bodylab functions
-def standard_healthiness_score(type: str, age: int, sex: str, weight: float, height=0) -> float:
-    if type is None or age is None or sex is None or weight is None:
-        return 'Some parameter has None value.'
+def standard_healthiness_score(score_type: str, age: int, sex: str, weight: float, height=0):
+  if score_type is None or age is None or sex is None or weight is None:
+    return 'Some parameter has None value.'
 
-    if type == 'fat_mass':
-        if sex == 'W':
-            if age < 18:
-                percent = 19.7
-            elif age < 21:
-                percent = (19.7 + 21.5) / 2
-            elif age < 26:
-                percent = 22.1
-            elif age < 31:
-                percent = 22.7
-            elif age < 36:
-                percent = (23.4 + 25.1) / 2
-            elif age < 41:
-                percent = (24.0 + 25.7) / 2
-            elif age < 46:
-                percent = (24.6 + 26.3) / 2
-            elif age < 51:
-                percent = 26.9
-            elif age < 56:
-                percent = 27.6
-            else:
-                percent = (28.2 + 29.8) / 2
-        elif sex == 'M':
-            if age < 18:
-                percent = 9.4
-            elif age < 21:
-                percent = 10.5
-            elif age < 26:
-                percent = 11.6
-            elif age < 31:
-                percent = (12.7 + 14.6) / 2
-            elif age < 36:
-                percent = (13.7 + 15.7) / 2
-            elif age < 41:
-                percent = 16.8
-            elif age < 46:
-                percent = 17.8
-            elif age < 51:
-                percent = (18.9 + 20.7) / 2
-            elif age < 56:
-                percent = (20.0 + 21.8) / 2
-            else:
-                percent = 22.8
-        else:
-            return 'Out of category: sex'
-        ideal_fat_mass = (weight * percent) / 100
-        return ideal_fat_mass  # float
-
-    elif type == 'muscle_mass':
-        if sex == 'W':
-            ideal_muscle_mass = weight * 0.34
-        elif sex == 'M':
-            ideal_muscle_mass = weight * 0.4
-        else:
-            return 'Out of category: sex'
-        return ideal_muscle_mass  # float
-
-    elif type == 'bmi_index':
-        if height != 0 and type(height) == float:
-            height_float = round(height / 100, 3)  # Scale of height in BMI index is 'meter'.
-            my_bmi = weight / (height_float ** 2)
-        else:
-            return 'Out of category: height'
-        return my_bmi  # float
-
+  if score_type == 'fat_mass':
+    if sex == 'W':
+      if age < 18:
+        percent = 19.7
+      elif age < 21:
+        percent = (19.7 + 21.5) / 2
+      elif age < 26:
+        percent = 22.1
+      elif age < 31:
+        percent = 22.7
+      elif age < 36:
+        percent = (23.4 + 25.1) / 2
+      elif age < 41:
+        percent = (24.0 + 25.7) / 2
+      elif age < 46:
+        percent = (24.6 + 26.3) / 2
+      elif age < 51:
+        percent = 26.9
+      elif age < 56:
+        percent = 27.6
+      else:
+        percent = (28.2 + 29.8) / 2
+    elif sex == 'M':
+      if age < 18:
+        percent = 9.4
+      elif age < 21:
+        percent = 10.5
+      elif age < 26:
+        percent = 11.6
+      elif age < 31:
+        percent = (12.7 + 14.6) / 2
+      elif age < 36:
+        percent = (13.7 + 15.7) / 2
+      elif age < 41:
+        percent = 16.8
+      elif age < 46:
+        percent = 17.8
+      elif age < 51:
+        percent = (18.9 + 20.7) / 2
+      elif age < 56:
+        percent = (20.0 + 21.8) / 2
+      else:
+        percent = 22.8
     else:
-        return 'Out of category: score type'
+      return 'Out of category: sex'
+    ideal_fat_mass = (weight * percent) / 100
+    return ideal_fat_mass  # float
 
+  elif score_type == 'muscle_mass':
+    if sex == 'W':
+      ideal_muscle_mass = weight * 0.34
+    elif sex == 'M':
+      ideal_muscle_mass = weight * 0.4
+    else:
+      return 'Out of category: sex'
+    return ideal_muscle_mass  # float
 
-# endregion
+  elif score_type == 'bmi_index':
+    if height != 0 and type(height) == float:
+      height_float = round(height / 100, 3)  # Scale of height in BMI index is 'meter'.
+      my_bmi = weight / (height_float ** 2)
+    else:
+      return 'Out of category: height'
+    return my_bmi  # float
+
+  else:
+    return 'Out of category: score type'
+
 
 def analyze_image(user_id, url):
-    response = requests.post(
-        f"http://{IMAGE_ANALYSYS_SERVER}/analysis",
-        json={
-            "user_id": user_id,
-            "url": url
-        })
+  response = requests.post(
+    f"http://{IMAGE_ANALYSYS_SERVER}/analysis",
+    json={
+      "user_id": user_id,
+      "url": url
+    })
 
-    if response.status_code == 200:
-        return json.dumps({'result': response.json(), 'status_code': 200}, ensure_ascii=False)
-    elif response.status_code == 400:
-        slack_error_notification(api='/api/bodylab/add', error_log=response.json()['message'])
-        return json.dumps({'error': response.json()['message'], 'status_code': 400}, ensure_ascii=False)
-    elif response.status_code == 500:
-        slack_error_notification(api='/api/bodylab/add', error_log=response.json()['message'])
-        return json.dumps({'error': response.json()['message'], 'status_code': 500}, ensure_ascii=False)
+  if response.status_code == 200:
+    return json.dumps({'result': response.json(), 'status_code': 200}, ensure_ascii=False)
+  elif response.status_code == 400:
+    slack_error_notification(api='/api/bodylab/add', error_log=response.json()['message'])
+    return json.dumps({'error': response.json()['message'], 'status_code': 400}, ensure_ascii=False)
+  elif response.status_code == 500:
+    slack_error_notification(api='/api/bodylab/add', error_log=response.json()['message'])
+    return json.dumps({'error': response.json()['message'], 'status_code': 500}, ensure_ascii=False)
+
+
+def get_date_range_from_week(year: str, week_number: str):
+  """
+  Reference: http://mvsourcecode.com/python-how-to-get-date-range-from-week-number-mvsourcecode/
+
+  getDateRangeFromWeek(2022, 2) ==> input<'week'> 기준 (2022, 1) (1.3 ~ 1.9)
+  getDateRangeFromWeek(2022, 53) ==> input<'week'> 기준 (2022, 52) (12.26 ~ 1.1)
+
+  getDateRangeFromWeek(2023, 2) ==> input<'week'> 기준 (2023, 1) (1.2 ~ 1.8)
+  getDateRangeFromWeek(2023, 53) ==> input<'week'> 기준 (2023, 52) (12.25 ~ 12.31)
+
+  getDateRangeFromWeek(2024, 2) ==> input<'week'> 기준 (2024, 1) (1.1 ~ 1.7)       *** getDateRangeFromWeek(2024, 1) == getDateRangeFromWeek(2024, 2)
+  getDateRangeFromWeek(2024, 53) ==> input<'week'> 기준 (2024, 52) (12.23 ~ 12.29)
+  ################################################################################### 이상 input_week + 1 == selected week
+  getDateRangeFromWeek(2025, 1) ==> input<'week'> 기준 (2025, 1) (12.30 ~ 1.5)
+  getDateRangeFromWeek(2025, 52) ==> input<'week'> 기준 (2025, 52) (12.22 ~ 12.28)
+
+  getDateRangeFromWeek(2026, 1) ==> input<'week'> 기준 (2026, 1) (12.29 ~ 1.4)
+  getDateRangeFromWeek(2026, 53) ==> input<'week'> 기준 (2026, 53) (12.28 ~ 1.3)
+  ################################################################################### 이상 input_week == selected week
+  getDateRangeFromWeek(2027, 2) ==> input<'week'> 기준 (2027, 1) (1.4 ~ 1.10)
+  getDateRangeFromWeek(2027, 53) ==> input<'week'> 기준 (2027, 52) (12.27 ~ 1.2)
+
+  getDateRangeFromWeek(2028, 2) ==> input<'week'> 기준 (2028, 1) (1.3 ~ 1.9)
+  getDateRangeFromWeek(2028, 53) ==> input<'week'> 기준 (2028, 52) (12.25 ~ 12.31)
+
+  getDateRangeFromWeek(2029, 2) ==> input<'week'> 기준 (2029, 1) (1.1 ~ 1.7)       *** getDateRangeFromWeek(2029, 1) == getDateRangeFromWeek(2029, 2)
+  getDateRangeFromWeek(2029, 53) ==> input<'week'> 기준 (2029, 52) (12.24 ~ 12.30)
+  ################################################################################### input_week + 1 == selected week
+  getDateRangeFromWeek(2030, 1) ==> input<'week'> 기준 (2030, 1) (12.31 ~ 1.6)
+  getDateRangeFromWeek(2030, 52) ==> input<'week'> 기준 (2030, 52) (12.23 ~ 12.29)
+
+  getDateRangeFromWeek(2031, 1) ==> input<'week'> 기준 (2031, 1) (12.30 ~ 1.5)
+  getDateRangeFromWeek(2031, 52) ==> input<'week'> 기준 (2031, 52) (12.22 ~ 12.28)
+
+  getDateRangeFromWeek(2032, 1) ==> input<'week'> 기준 (2032, 1) (12.29 ~ 1.4)
+  getDateRangeFromWeek(2032, 53) ==> input<'week'> 기준 (2032, 53) (12.27 ~ 1.2)
+  ################################################################################### 이상 input_week == selected week
+  getDateRangeFromWeek(2033, 2) ==> input<'week'> 기준 (2033, 1) (1.3 ~ 1.9)
+  getDateRangeFromWeek(2033, 53) ==> input<'week'> 기준 (2033, 52) (12.26 ~ 1.1)
+
+  getDateRangeFromWeek(2034, 2) ==> input<'week'> 기준 (2034, 1) (1.2 ~ 1.8)
+  getDateRangeFromWeek(2034, 53) ==> input<'week'> 기준 (2034, 52) (12.25 ~ 12.31)
+
+  getDateRangeFromWeek(2035, 2) ==> input<'week'> 기준 (2035, 1) (1.1 ~ 1.7)      *** getDateRangeFromWeek(2035, 1) == getDateRangeFromWeek(2035, 2)
+  getDateRangeFromWeek(2035, 53) ==> input<'week'> 기준 (2035, 52) (12.24 ~ 12.30)
+  ################################################################################### input_week + 1 == selected week
+  getDateRangeFromWeek(2036, 1) ==> input<'week'> 기준 (2036, 1) (12.31 ~ 1.6)
+  getDateRangeFromWeek(2036, 52) ==> input<'week'> 기준 (2036, 52) (12.22 ~ 12.28)
+
+  :param year: year(YYYY)
+  :param week_number: month(mm)
+  # Either (String, String) or (int, int) is OK.
+  # But month format is 'ww', so if value 01~09 and
+  # you want to set parameter type as int, you must convert string 'ww' to int 'w'.
+  :return:
+  """
+  if year in ['2022', '2023', '2024', '2027', '2028', '2029', '2033', '2034', '2035']:
+    corrected_week_number = int(week_number) + 1  # int("01" ~ "09) => 1 ~ 9
+
+    firstdate_of_week = datetime.datetime.strptime(f'{year}-W{int(corrected_week_number)-1}-1', "%Y-W%W-%w").date()
+    lastdate_of_week = firstdate_of_week + datetime.timedelta(days=6.9)
+    return int(year), int(corrected_week_number), firstdate_of_week, lastdate_of_week
+  elif year in ['2025', '2026', '2030', '2031', '2032', '2036']:
+    firstdate_of_week = datetime.datetime.strptime(f'{year}-W{int(week_number)-1}-1', "%Y-W%W-%w").date()
+    lastdate_of_week = firstdate_of_week + datetime.timedelta(days=6.9)
+    return int(year), int(week_number), firstdate_of_week, lastdate_of_week
+
+# endregion
