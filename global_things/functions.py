45d7b89534b729f38b76cb34151a2857ff3b3c95
circlindev-caleb <caleb@circlin.co.kr>
2022-01-20 22:57:30 +0900

    Add slack notification when purchase(INSERT) occurs.

--- global_things/functions.py
+++ global_things/functions.py
@@ -91,7 +91,7 @@ def slack_purchase_notification(cursor, user_id: int = 0, manager_id: int = 0, p
     json.dumps({
       "channel": "#circlin-plus-order-log",
       "username": f"결제 완료 알림: {nickname}({user_id})",
-      "text": f":dollar::dollar:{nickname} 고객님께서 *{plan_title}* 플랜을 결제하셨습니다!:dollar::dollar: \n\n \
+      "text": f":dollar::dollar: {nickname} 고객님께서 *{plan_title}* 플랜을 결제하셨습니다! :dollar::dollar: \n\n \
 결제 id: `{purchase_id}` \n \
 시작일: `{start_date.strftime('%Y-%m-%d %H:%M:%S')}` \n \
 만료일: `{expire_date.strftime('%Y-%m-%d %H:%M:%S')}`\n \
