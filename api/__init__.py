75d4be4d24fae3e901c3167a22a6964d7876450e
circlindev-caleb <caleb@circlin.co.kr>
2022-01-21 19:54:23 +0900

    Adding explore.py(filter == 'program').

--- api/__init__.py
+++ api/__init__.py
@@ -1,6 +1,7 @@
 from flask import Blueprint
 from flask_cors import CORS
-from . import bodylab, user_question, purchase, explore
 
 api = Blueprint('api', __name__)
 CORS(api)
+
+from . import bodylab, user_question, purchase, explore
