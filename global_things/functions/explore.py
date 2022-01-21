b16f970cff68fc1926be12a0861ff366eeb0a4c5
circlindev-caleb <caleb@circlin.co.kr>
2022-01-21 19:57:59 +0900

    Adding explore.py(filter == 'program').

--- global_things/functions/explore.py
+++ global_things/functions/explore.py
@@ -4,10 +4,9 @@ def explore_query(filter, word):
       SELECT
             p.id AS program_id,
             p.title,
-            (SELECT pathname FROM files WHERE id = p.thumbnail_id) AS thumbnail            
+            (SELECT pathname FROM files WHERE id = p.thumbnail_id) AS thumbnail,          
             f.pathname AS thumbnails,
-            (SELECT COUNT(*) FROM program_lectures WHERE program_id = program_id) AS num_lectures,
-
+            (SELECT COUNT(*) FROM program_lectures WHERE program_id = program_id) AS num_lectures
         FROM
             programs p,
             files f
