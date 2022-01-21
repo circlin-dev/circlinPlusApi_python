667705cbe383d4d28f330778beef92d6b43743ef
circlindev-caleb <caleb@circlin.co.kr>
2022-01-21 19:48:12 +0900

    Adding explore.py(filter == 'program').

--- api/__init__.py
+++ api/__init__.py
@@ -1,7 +1,6 @@
 from flask import Blueprint
 from flask_cors import CORS
+from . import bodylab, user_question, purchase, explore
 
 api = Blueprint('api', __name__)
 CORS(api)
-
-from . import bodylab, user_question, purchase, explore
--- api/explore.py
+++ api/explore.py
@@ -1,23 +1,87 @@
+from global_things.functions.slack import slack_error_notification
+from global_things.functions.general import login_to_db, check_user, query_result_is_none
 from . import api
-from global_things.constants import IMAGE_ANALYSYS_SERVER, SLACK_NOTIFICATION_WEBHOOK
-from config.database import DB_CONFIG
 import datetime
 from flask import request
 import json
+import pandas as pd
 import pymysql
-from pymysql.converters import escape_string
 import requests
 
 
 @api.route('/explore/<filter>/<word>', methods=['GET'])
 def explore(filter, word):
-  '''
-
-  :param filter: 필터명(잠정: 운동, 강사, 기구, 목표
+  """
+  :param filter: 필터명(잠정: program, exercise, coach, equipment, purpose)
   :param word: 검색어
-  :return:
-  '''
+  :return: {"id": 0,
+            "title": "",
+            "thumbnail": "",
+            "thumbnails": [{pathname: "320w.png"}, {pathname: "240w.png",}],
+            "num_lectures": 0}
+  """
   ip = request.remote_addr
-  endpoint = '/explore/<word>'
+  endpoint = '/explore/<filter>/<word>'
 
-  return None
+  try:
+    connection = login_to_db()
+  except Exception as e:
+    error = str(e)
+    result = {
+      'result': False,
+      'error': f'Server Error while connecting to DB: {error}'
+    }
+    slack_error_notification(user_ip=ip, api=endpoint, error_log=result['error'])
+    return json.dumps(result, ensure_ascii=False), 500
+
+  cursor = connection.cursor()
+
+  if filter == 'program':
+    query = f"""
+      SELECT
+            p.id AS program_id,
+            p.title,
+            (SELECT pathname FROM files WHERE id = p.thumbnail_id) AS thumbnail            
+            f.pathname AS thumbnails,
+            (SELECT COUNT(*) FROM program_lectures WHERE program_id = 1) AS num_lectures,
+    
+        FROM
+            programs p,
+            files f
+        WHERE 
+            p.title LIKE "%{word}%"
+        AND
+            f.original_file_id = p.thumbnail_id"""
+  else:
+    pass
+
+  cursor.execute(query)
+  programs_df = pd.DataFrame(cursor.fetchall(), columns=['program_id', 'title', 'thumbnail', 'thumbnails', 'num_lectures'])
+  program_ids = programs_df['program_id'].unique()
+
+  result_list = []
+  for each_id in program_ids:
+    df_by_id = programs_df[programs_df['program_id'] == each_id]
+    title = df_by_id['title'].unique()[0]
+    thumbnail = df_by_id['thumbnail'].unique()[0]
+    thumbnails = df_by_id['thumbnails'].sort_values(ascending=True)  # Thumbnail needs be sorted from big size to small size(1080 -> ... 150).
+    num_lectures = df_by_id['num_lectures'].unique()[0]
+    thumbnails_list = []
+    for image in thumbnails:
+      each_dict = {"pathname": image}
+      thumbnails.append(each_dict)
+
+    result = {
+      "id": each_id,
+      "title": title,
+      "thumbnail": thumbnail,
+      "thumbnails": thumbnails,
+      "num_lectures": num_lectures
+    }
+    result_list.append(result)
+
+  result_dict = {
+    "result": True,
+    "search_results": result_list
+  }
+  return json.dumps(result_dict, ensure_ascii=False), 200
