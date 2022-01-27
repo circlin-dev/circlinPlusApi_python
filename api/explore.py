a189b309e615e4abd820d521281c01a2031ad285
circlindev-caleb <caleb@circlin.co.kr>
2022-01-27 11:36:35 +0900

    Implemented explore.py: Searching programs by coach name, exercise, equipment.

--- api/explore.py
+++ api/explore.py
@@ -1,33 +1,26 @@
 from global_things.constants import API_ROOT
 from global_things.functions.slack import slack_error_notification
-from global_things.functions.explore import explore_query
-from global_things.functions.general import login_to_db, check_token, query_result_is_none
+from global_things.functions.explore import explore_query, filtering_dataframe
+from global_things.functions.general import login_to_db, check_token
 from . import api
-import datetime
+from collections import OrderedDict
 from flask import url_for, request
 import json
 import pandas as pd
-import pymysql
-import requests
 
 
 @api.route('/explore', methods=['POST'])
 def explore():
   """
-  :param search_filter: 필터명(잠정: program, exercise, coach, equipment, purpose)
-  :param word: 검색어
-  :return: {"id": 0,
-            "title": "",
-            "thumbnail": "",
-            "thumbnails": [{pathname: "320w.png"}, {pathname: "240w.png",}],
-            "num_lectures": 0}
+  header: Authorization
+  body: filter{exercises, purposes, equipments}, sort_by, word,
   """
   # ip = request.environ.get('HTTP_X_REAL_IP', request.remote_addr)
   ip = request.headers["X-Forwarded-For"]  # Both public & private.
   endpoint = API_ROOT + url_for('api.explore')
   # token = request.headers['Authorization']
   parameters = json.loads(request.get_data(), encoding='utf-8')
-  # {"filter": {"exercises": [], "purposes": [], "equipments": []}, "word": "", "sort_by": ""}
+  # 필터: {"filter": {"exercises": [], "purposes": [], "equipments": []}, "word": "", "sort_by": ""}
   filter_list_exercises = parameters['filter']['exercise']  # default: Everything
   filter_list_purposes = parameters['filter']['purposes']  # default: everything
   filter_list_equipments = parameters['filter']['equipments']
@@ -46,61 +39,33 @@ def explore():
     return json.dumps(result, ensure_ascii=False), 500
 
   cursor = connection.cursor()
+  query_program, query_coach, query_exercise, query_equipment = explore_query(word_for_search, sort_by)
 
-  query = explore_query(word_for_search, sort_by)
-  cursor.execute(query)
+  cursor.execute(query_program)
+  programs_by_program = pd.DataFrame(cursor.fetchall(), columns=['program_id', 'created_at', 'title',
+                                                                 'exercise', 'equipments', 'purposes',
+                                                                 'thumbnail', 'thumbnails', 'num_lectures'])
+  program_list_by_program = filtering_dataframe(filter_list_exercises, filter_list_purposes, filter_list_equipments, programs_by_program)
 
-  programs_df = pd.DataFrame(cursor.fetchall(), columns=['program_id', 'created_at', 'title',
-                                                         'exercise', 'equipments', 'purposes',
-                                                         'thumbnail', 'thumbnails', 'num_lectures'])
+  cursor.execute(query_coach)
+  programs_by_coach = pd.DataFrame(cursor.fetchall(), columns=['program_id', 'created_at', 'title',
+                                                               'exercise', 'equipments', 'purposes',
+                                                               'thumbnail', 'thumbnails', 'num_lectures'])
+  program_list_by_coach = filtering_dataframe(filter_list_exercises, filter_list_purposes, filter_list_equipments, programs_by_coach)
 
-  # Apply search filter ==> 필터 순서에 따라 결과가 달라질까???
-  if len(filter_list_exercises) == 0 and len(filter_list_purposes) == 0 and len(filter_list_equipments) == 0:
-    pass
-  elif len(filter_list_exercises) > 0 and len(filter_list_purposes) == 0 and len(filter_list_equipments) == 0:
-    programs_df = programs_df[programs_df['exercise'].isin(filter_list_exercises)]
-  elif len(filter_list_exercises) == 0 and len(filter_list_purposes) > 0 and len(filter_list_equipments) == 0:
-    programs_df = programs_df[programs_df['purposes'].isin(filter_list_purposes)]
-  elif len(filter_list_exercises) == 0 and len(filter_list_purposes) == 0 and len(filter_list_equipments) > 0:
-    programs_df = programs_df[programs_df['equipments'].isin(filter_list_equipments)]
-  elif len(filter_list_exercises) > 0 and len(filter_list_purposes) > 0 and len(filter_list_equipments) == 0:
-    programs_df = programs_df[programs_df['exercise'].isin(filter_list_exercises) &
-                              programs_df['purposes'].isin(filter_list_purposes)]
-  elif len(filter_list_exercises) > 0 and len(filter_list_purposes) == 0 and len(filter_list_equipments) > 0:
-    programs_df = programs_df[programs_df['exercise'].isin(filter_list_exercises) &
-                              programs_df['equipments'].isin(filter_list_equipments)]
-  elif len(filter_list_exercises) == 0 and len(filter_list_purposes) > 0 and len(filter_list_equipments) > 0:
-    programs_df = programs_df[programs_df['purposes'].isin(filter_list_purposes) &
-                              programs_df['equipments'].isin(filter_list_equipments)]
-  else:
-    programs_df = programs_df[programs_df['exercise'].isin(filter_list_exercises) &
-                              programs_df['purposes'].isin(filter_list_purposes) &
-                              programs_df['equipments'].isin(filter_list_equipments)]
+  cursor.execute(query_exercise)
+  programs_by_exercise = pd.DataFrame(cursor.fetchall(), columns=['program_id', 'created_at', 'title',
+                                                                  'exercise', 'equipments', 'purposes',
+                                                                  'thumbnail', 'thumbnails', 'num_lectures'])
+  program_list_by_exercise = filtering_dataframe(filter_list_exercises, filter_list_purposes, filter_list_equipments, programs_by_exercise)
 
-  program_ids = programs_df['program_id'].unique()
+  cursor.execute(query_equipment)
+  programs_by_equipment = pd.DataFrame(cursor.fetchall(), columns=['program_id', 'created_at', 'title',
+                                                                   'exercise', 'equipments', 'purposes',
+                                                                   'thumbnail', 'thumbnails', 'num_lectures'])
+  program_list_by_equipment = filtering_dataframe(filter_list_exercises, filter_list_purposes, filter_list_equipments, programs_by_equipment)
 
-  result_list = []
-  for each_id in program_ids:
-    each_id = int(each_id)
-    df_by_id = programs_df[programs_df['program_id'] == each_id]
-    title = df_by_id['title'].unique()[0]  # For error 'TypeError: Object of type int64 is not JSON serializable'
-    thumbnail = df_by_id['thumbnail'].unique()[0]
-    thumbnails = list(set(df_by_id['thumbnails'].values.tolist()[0].strip('][').split(', ')))
-    thumbnails = sorted(thumbnails, key=lambda x: int(x.split('_')[1].split('w')[0]), reverse=True)  # Thumbnails needs be sorted from big size to small size(1080 -> ... 150).
-    num_lectures = int(df_by_id['num_lectures'].unique()[0])
-    thumbnails_list = []
-    for image in thumbnails:
-      thumbnails_list.append(json.loads(image))
-
-    result = {
-      "id": each_id,
-      "title": title,
-      "thumbnail": thumbnail,
-      "thumbnails": thumbnails_list,
-      "num_lectures": num_lectures
-    }
-    result_list.append(result)
-
+  result_list = list(OrderedDict.fromkeys(program_list_by_program + program_list_by_coach + program_list_by_exercise + program_list_by_equipment))
   result_dict = {
     "result": True,
     "search_results": result_list
--- global_things/functions/explore.py
+++ global_things/functions/explore.py
@@ -1,4 +1,5 @@
 import json
+import pandas as pd
 
 # def explore_query(search_filter, word):
 #   if search_filter == 'program':
@@ -37,7 +38,6 @@ def explore_query(word: str = "", sort_by: str = "latest"):
     (2) ex.title LIKE "%피%"
     (3) eq.name LIKE "%피%"
     (4) pur.title LIKE "%피%"
-  :param search_filter:
   :param word:
   :param sort_by:
   :return:
@@ -50,7 +50,7 @@ def explore_query(word: str = "", sort_by: str = "latest"):
   else:
     sort_standard = "p.created_at"
 
-  query = f"""
+  query_program = f"""
     SELECT
           p.id AS program_id,
           p.created_at,
@@ -84,7 +84,119 @@ def explore_query(word: str = "", sort_by: str = "latest"):
           p.id, ex.id, eq.id, pur.id
     ORDER BY 
           {sort_standard} DESC"""
-  return query
+
+  query_coach = f"""
+    SELECT
+          p.id AS program_id,
+          p.created_at,
+          p.title,
+          ex.title AS exercise,
+          eq.name AS equipments,
+          pur.title AS purposes,
+          (SELECT pathname FROM files WHERE id = p.thumbnail_id) AS thumbnail,
+          JSON_ARRAYAGG(JSON_OBJECT('pathname', f.pathname)) AS thumbnails,
+          (SELECT COUNT(*) FROM program_lectures WHERE program_id = p.id) AS num_lectures
+      FROM
+          programs p,
+          files f,
+          coaches AS c,
+          exercises ex,
+          program_exercises pex,
+          equipments eq,
+          program_equipments peq,
+          purposes pur,
+          program_purposes ppu
+      WHERE
+          c.name LIKE "%{word}%"
+        AND 
+          p.coach_id = (SELECT id FROM coaches WHERE name=c.name)      
+        AND
+          f.original_file_id = p.thumbnail_id
+        AND
+          (ex.id = pex.exercise_id AND p.id = pex.program_id)
+        AND
+          (eq.id = peq.equipment_id AND p.id = peq.program_id)
+        AND
+          (pur.id = ppu.purpose_id AND ppu.program_id = p.id)
+    GROUP BY 
+          p.id, ex.id, eq.id, pur.id
+    ORDER BY 
+          {sort_standard} DESC"""
+
+  query_exercise = f"""
+    SELECT
+          p.id AS program_id,
+          p.created_at,
+          p.title,
+          ex.title AS exercise,
+          eq.name AS equipments,
+          pur.title AS purposes,
+          (SELECT pathname FROM files WHERE id = p.thumbnail_id) AS thumbnail,
+          JSON_ARRAYAGG(JSON_OBJECT('pathname', f.pathname)) AS thumbnails,
+          (SELECT COUNT(*) FROM program_lectures WHERE program_id = p.id) AS num_lectures
+      FROM
+          programs p,
+          files f,
+          exercises ex,
+          program_exercises pex,
+          equipments eq,
+          program_equipments peq,
+          purposes pur,
+          program_purposes ppu
+      WHERE
+          ex.title LIKE "%{word}%"
+        AND
+          pex.exercise_id = (SELECT id FROM exercises WHERE title = ex.title)
+        AND
+          f.original_file_id = p.thumbnail_id
+        AND
+          (ex.id = pex.exercise_id AND p.id = pex.program_id)
+        AND
+          (eq.id = peq.equipment_id AND p.id = peq.program_id)
+        AND
+          (pur.id = ppu.purpose_id AND ppu.program_id = p.id)
+    GROUP BY 
+          p.id, ex.id, eq.id, pur.id
+    ORDER BY 
+          {sort_standard} DESC"""
+
+  query_equipment = f"""
+    SELECT
+          p.id AS program_id,
+          p.created_at,
+          p.title,
+          ex.title AS exercise,
+          eq.name AS equipments,
+          pur.title AS purposes,
+          (SELECT pathname FROM files WHERE id = p.thumbnail_id) AS thumbnail,
+          JSON_ARRAYAGG(JSON_OBJECT('pathname', f.pathname)) AS thumbnails,
+          (SELECT COUNT(*) FROM program_lectures WHERE program_id = p.id) AS num_lectures
+      FROM
+          programs p,
+          files f,
+          exercises ex,
+          program_exercises pex,
+          equipments eq,
+          program_equipments peq,
+          purposes pur,
+          program_purposes ppu
+      WHERE
+          eq.name LIKE "%{word}%"
+        AND
+          peq.equipment_id = (SELECT id FROM equipments WHERE name = eq.name)
+        AND
+          f.original_file_id = p.thumbnail_id
+        AND
+          (ex.id = pex.exercise_id AND p.id = pex.program_id)
+        AND
+          (eq.id = peq.equipment_id AND p.id = peq.program_id)
+        AND
+          (pur.id = ppu.purpose_id AND ppu.program_id = p.id)
+    GROUP BY 
+          p.id, ex.id, eq.id, pur.id
+    ORDER BY 
+          {sort_standard} DESC"""
+  return query_program, query_coach, query_exercise, query_equipment
 
   # elif search_filter == 'exercise':
   #   pass
@@ -96,3 +208,55 @@ def explore_query(word: str = "", sort_by: str = "latest"):
   #   pass
   # else:
   #   pass
+
+
+def filtering_dataframe(filter_exercise: list, filter_purpose: list, filter_equipment: list, programs_df: pd.DataFrame):
+  programs = programs_df.copy()
+  # Apply search filter ==> 필터 순서에 따라 결과가 달라질까???
+  if len(filter_exercise) == 0 and len(filter_purpose) == 0 and len(filter_equipment) == 0:
+    pass
+  elif len(filter_exercise) > 0 and len(filter_purpose) == 0 and len(filter_equipment) == 0:
+    programs = programs[programs['exercise'].isin(filter_exercise)]
+  elif len(filter_exercise) == 0 and len(filter_purpose) > 0 and len(filter_equipment) == 0:
+    programs = programs[programs['purposes'].isin(filter_purpose)]
+  elif len(filter_exercise) == 0 and len(filter_purpose) == 0 and len(filter_equipment) > 0:
+    programs = programs[programs['equipments'].isin(filter_equipment)]
+  elif len(filter_exercise) > 0 and len(filter_purpose) > 0 and len(filter_equipment) == 0:
+    programs = programs[programs['exercise'].isin(filter_exercise) &
+                        programs['purposes'].isin(filter_purpose)]
+  elif len(filter_exercise) > 0 and len(filter_purpose) == 0 and len(filter_equipment) > 0:
+    programs = programs[programs['exercise'].isin(filter_exercise) &
+                        programs['equipments'].isin(filter_equipment)]
+  elif len(filter_exercise) == 0 and len(filter_purpose) > 0 and len(filter_equipment) > 0:
+    programs = programs[programs['purposes'].isin(filter_purpose) &
+                        programs['equipments'].isin(filter_equipment)]
+  else:
+    programs = programs[programs['exercise'].isin(filter_exercise) &
+                        programs['purposes'].isin(filter_purpose) &
+                        programs['equipments'].isin(filter_equipment)]
+
+  program_ids = programs['program_id'].unique()
+
+  result_list = []
+  for each_id in program_ids:
+    each_id = int(each_id)
+    df_by_id = programs[programs['program_id'] == each_id]
+    title = df_by_id['title'].unique()[0]  # For error 'TypeError: Object of type int64 is not JSON serializable'
+    thumbnail = df_by_id['thumbnail'].unique()[0]
+    thumbnails = list(set(df_by_id['thumbnails'].values.tolist()[0].strip('][').split(', ')))
+    thumbnails = sorted(thumbnails, key=lambda x: int(x.split('_')[1].split('w')[0]), reverse=True)  # Thumbnails needs be sorted from big size to small size(1080 -> ... 150).
+    num_lectures = int(df_by_id['num_lectures'].unique()[0])
+    thumbnails_list = []
+    for image in thumbnails:
+      thumbnails_list.append(json.loads(image))
+
+    result = {
+      "id": each_id,
+      "title": title,
+      "thumbnail": thumbnail,
+      "thumbnails": thumbnails_list,
+      "num_lectures": num_lectures
+    }
+    result_list.append(result)
+
+  return result_list
