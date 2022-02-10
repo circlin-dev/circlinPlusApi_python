83665765fe5e883b8d0740ae04771c8b7df9baa3
circlindev-caleb <caleb@circlin.co.kr>
2022-02-11 02:47:47 +0900

    Added products.py for products.

--- api/products.py
+++ api/products.py
@@ -96,7 +96,7 @@ def read_products():
                                                            'name', 'description', 'brand_name',
                                                            'price_origin', 'price_sales', 'quantity',
                                                            'thumbnail', 'details'])
-    products_df['details'] = products_df['details'].apply(lambda x: list(set(ast.literal_eval(x.strip('][').split(', ')))))
+    products_df['details'] = products_df['details'].apply(lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
     products_df['details'] = products_df['details'].apply(lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
 
     result_dict = json.loads(products_df.to_json(orient='records'))
