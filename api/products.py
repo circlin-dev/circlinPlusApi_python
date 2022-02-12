872ad71c2c129ae4b42a9150aea3e3593fa2857b
circlindev-caleb <caleb@circlin.co.kr>
2022-02-12 16:44:17 +0900

    Changed reference table when getting a single product's images.

--- api/products.py
+++ api/products.py
@@ -96,25 +96,28 @@ def read_products():
         WHERE p.`type` = '{parameters[0]}'
         GROUP BY p.id"""
     cursor.execute(sql)
+    result = cursor.fetchall()
+    if query_result_is_none(result):
+        connection.close()
+        result_dict = {}
+        return json.dumps(result_dict, ensure_ascii=False), 200
 
     products_df = pd.DataFrame(cursor.fetchall(), columns=['id', 'type', 'code',
                                                            'name', 'description', 'brand_name',
                                                            'price_origin', 'price_sales', 'quantity',
                                                            'thumbnail', 'details'])
-    try:
-        products_df['details'] = products_df['details'].apply(lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
-        products_df['details'] = products_df['details'].apply(lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
-    except:
-        """products_df의 컬럼값이 None일 때 => 검색 결과가 없는 경우일 수도 있고, 썸네일만 없는 경우일 수도 있다."""
-        connection.close()
-        result_dict = json.loads(products_df.to_json(orient='records'))  # Array type으로 가고있음
-        return json.dumps(result_dict, ensure_ascii=False), 200
-        # if products_df['id'] is None:  # 검색 결과가 없는 경우
-        #     result_dict = {}
-        #     return json.dumps(result_dict, ensure_ascii=False), 200
-        # else:  # 썸네일만 없는 경우.
-        #     result_dict = json.loads(products_df.to_json(orient='records'))  # Array type으로 가고있음
-        #     return json.dumps(result_dict, ensure_ascii=False), 200
+    products_df['details'] = products_df['details'].apply(
+        lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
+    products_df['details'] = products_df['details'].apply(
+        lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
+    # try:
+    #     products_df['details'] = products_df['details'].apply(lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
+    #     products_df['details'] = products_df['details'].apply(lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
+    # except:
+    #     """products_df의 컬럼값이 None일 때 => 검색 결과가 없는 경우일 수도 있고, 썸네일만 없는 경우일 수도 있다."""
+    #     connection.close()
+    #     result_dict = json.loads(products_df.to_json(orient='records'))  # Array type으로 가고있음
+    #     return json.dumps(result_dict, ensure_ascii=False), 200
 
     connection.close()
     result_dict = json.loads(products_df.to_json(orient='records'))  # Array type으로 가고있음
