71e72fe871ca014e258cd5ca6dbf67a627ee5f9e
circlindev-caleb <caleb@circlin.co.kr>
2022-02-12 16:45:28 +0900

    Changed reference table when getting a single product's images.

--- api/products.py
+++ api/products.py
@@ -109,7 +109,7 @@ def read_products():
     products_df['details'] = products_df['details'].apply(
         lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
     products_df['details'] = products_df['details'].apply(
-        lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
+        lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')))
     # try:
     #     products_df['details'] = products_df['details'].apply(lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
     #     products_df['details'] = products_df['details'].apply(lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
