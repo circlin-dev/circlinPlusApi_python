d8c61d32d0904c372a48567b9bdae799134527f9
circlindev-caleb <caleb@circlin.co.kr>
2022-02-12 16:51:05 +0900

    Changed reference table when getting a single product's images.

--- api/products.py
+++ api/products.py
@@ -97,6 +97,7 @@ def read_products():
         GROUP BY p.id"""
     cursor.execute(sql)
     result = cursor.fetchall()
+
     if query_result_is_none(result):
         connection.close()
         result_dict = {}
@@ -106,13 +107,18 @@ def read_products():
                                                 'name', 'description', 'brand_name',
                                                 'price_origin', 'price_sales', 'quantity',
                                                 'thumbnail', 'details'])
-    products_df['details'] = products_df['details'].apply(
-        lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
-    products_df['details'] = products_df['details'].apply(
-        lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
+    try:
+        products_df['details'] = products_df['details'].apply(
+            lambda x: [ast.literal_eval(el) for el in list(set(x.strip('][').split(', ')))])
+        products_df['details'] = products_df['details'].apply(
+            lambda x: sorted(x, key=lambda y: y.split('/')[-1].split('_')[-1].split('.')[0]))
+    except:
+        connection.close()
+        result_dict = json.loads(products_df.to_json(orient='records'))  # Array type으로 가고있음
+        return json.dumps(result_dict, ensure_ascii=False), 200
 
     connection.close()
-    result_dict = json.loads(products_df.to_json(orient='records'))[0]  # Array type으로 가고있음
+    result_dict = json.loads(products_df.to_json(orient='records'))  # Array type으로 가고있음
     return json.dumps(result_dict, ensure_ascii=False), 200
 
 
