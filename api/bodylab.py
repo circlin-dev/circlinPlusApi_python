3c1bc61698d8ce0cb89575e149e229a15608f566
circlindev-caleb <caleb@circlin.co.kr>
2022-02-17 22:43:53 +0900

    INSERT & SELECT additional and data bodylab.

--- api/bodylab.py
+++ api/bodylab.py
@@ -438,8 +438,11 @@ def read_user_bodylab_single(user_id, bodylab_id):
                "height",
                "weight",
                "bmi",
+               "bmi_status",
                "muscle_mass",
+               "ideal_muscle_mass",
                "fat_mass",
+               "ideal_fat_mass"
                "url_output",
                "shoulder_ratio",
                "hip_ratio",
