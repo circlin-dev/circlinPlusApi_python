89d74ebe0b736e08da523d0809589af976b22168
circlindev-caleb <caleb@circlin.co.kr>
2022-02-25 06:02:07 +0900

    Fixing equipments.py

--- api/products.py
+++ api/products.py
@@ -169,7 +169,7 @@ def read_products():
         products_df['related_programs'] = products_df['related_programs'].apply(lambda x: list({data['id']: data for data in x}.values()))
         products_df['related_programs'] = products_df['related_programs'].apply(lambda x: [] if x[0]['id'] is None else x)
 
-        products_df = products_df.sort_values(by=['stocks', 'price'], ascending=False)
+        products_df = products_df.sort_values(by=['stocks', 'price'], ascending=True)
         sorter = ['on_sale', 'future', 'sold_out_temp', 'sold_out']
         products_df.status = products_df.status.astype('category')
         products_df.status.cat.set_categories(sorter)
