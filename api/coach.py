fb949477046c8f9c2f2a524207204b35d3999e2a
circlindev-caleb <caleb@circlin.co.kr>
2022-02-25 03:25:05 +0900

    Make coach api.

--- api/coach.py
+++ api/coach.py
@@ -36,8 +36,8 @@ def get_coaches():
             'id', f.id, 
             'thumbnail', f.pathname
         ) AS coach_thumbnail,
-        c.greeting AS introducing,
-        c.category AS related_exercise,
+        c.greeting AS description,
+        c.category AS exercise,
         c.affiliation AS team,
         JSON_OBJECT(
             'id', p.id, 
@@ -54,7 +54,8 @@ def get_coaches():
             'id', prod.id,
             'title', prod.title,
             'thumbnail', (SELECT files.pathname FROM files WHERE files.id=(SELECT file_id FROM product_images WHERE product_id = prod.id AND type='thumbnail'))
-        ) AS related_product
+        ) AS related_product,
+        (SELECT files.pathname FROM files WHERE files.id=c.intro_id) AS intro
     FROM
          coaches c
     LEFT JOIN
@@ -86,45 +87,29 @@ def get_coaches():
     WHERE c.deleted_at IS NULL
     GROUP BY c.id"""
     cursor.execute(sql)
-    coaches = pd.DataFrame(cursor.fetchall(),
-                           columns=['id', 'name', 'thumbnail', 'introducing',
-                                    'exercise', 'team', 'related_program',
-                                    'release_at', 'status', 'tag', 'related_product'])
-    coach_ids = coaches['id'].unique()
-    result_list = []
-    for each_id in coach_ids:
-        each_id = int(each_id)
-        df_by_id = coaches[coaches['id'] == each_id]
-        thumbnail = json.loads(df_by_id['thumbnail'].unique()[0])
-        introducing = df_by_id['introducing'].unique()[0]
-        exercise = df_by_id['exercise'].unique()[0]
-        team = df_by_id['team'].unique()[0]
-        related_program = json.loads(df_by_id['related_program'].unique()[0])
-        if df_by_id['release_at'].unique()[0].dt is None:
-            release_at = None
-        else:
-            release_at = pd.to_datetime(df_by_id['release_at'].unique()[0]).dt.strftime('%Y-%m-%d %H:%M:%S')
-        status = df_by_id['status'].unique()[0]
-        tag_list = json.loads(df_by_id['tag'].unique()[0])
-        related_product = json.loads(df_by_id['related_product'].unique()[0])
+    coaches = cursor.fetchall()
 
+    result_list = []
+    for coach in coaches:
         result_dict = {
-            "id": each_id,
-            "thumbnail": thumbnail,
-            "introducing": introducing,
-            "exercise": exercise,
-            "team": team,
-            "related_program": related_program,
-            "release_at": release_at,
-            "status": status,
-            "tag_list": tag_list,
-            "related_product": related_product
+            "id": coach[0],
+            "title": coach[1],
+            "thumbnail": json.loads(coach[2]),
+            "description": coach[3],
+            "exercise": coach[4],
+            "team": coach[5],
+            "related_program": coach[6],
+            "release_at": coach[7].strftime('%Y-%m-%d %H:%M:%S'),
+            "status": coach[8],
+            "tag_list": json.loads(coach[9]),
+            "related_product": json.loads(coach[10]),
+            "intro": coach[11]
         }
         result_list.append(result_dict)
 
     result = {
         'result': True,
-        'data': result_list
+        'data': result_dict
     }
     return json.dumps(result, ensure_ascii=False), 200
 
