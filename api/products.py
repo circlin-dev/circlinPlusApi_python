36078658dee6e61af785b7a345f5a73ea8f67222
circlindev-caleb <caleb@circlin.co.kr>
2022-03-02 19:31:26 +0900

    Reverting products.py

--- api/products.py
+++ api/products.py
@@ -124,91 +124,91 @@ def read_products():
             GROUP BY prod.id, prog.id"""
     else:
         sql = f"""
-            SELECT
-               prod.id,
-               prod.type,
-               prod.code,
-               prod.title as title,
-               prod.description,
-               b.title as brandTitle,
-               prod.original_price as original_price,
-               prod.price as price,
-               CASE
-                   WHEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0)) <= 0 THEN 0
-                   WHEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0)) > 0 THEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0))
-               END AS stock,
-               (
-                   SELECT
-                       f2.pathname
-                   FROM
-                        files f2
-                    INNER JOIN product_images pi2
-                   ON pi2.file_id = f2.id
-                    WHERE pi2.type='thumbnail' AND pi2.product_id=prod.id
-                ) AS thumbnail,
-               (
-                   SELECT JSON_ARRAYAGG(pathname) FROM files
-                       INNER JOIN product_images
-                            ON product_images.file_id = files.original_file_id
-                   WHERE product_images.product_id = prod.id AND product_images.type = 'thumbnail'
-               ) AS thumbnails,
-               (
-                   SELECT JSON_ARRAYAGG(pathname) FROM files
-                       INNER JOIN product_images
-                           ON product_images.file_id = files.id
-                   WHERE product_images.type = 'detail' AND product_images.product_id = prod.id
-               ) AS details,
-               (
-                   SELECT JSON_ARRAYAGG(pathname) FROM files
-                       INNER JOIN product_images
-                            ON product_images.file_id = files.original_file_id
-                   WHERE product_images.type = 'detail' AND product_images.product_id = prod.id
-               ) AS detail_thumbnails,
-               JSON_ARRAYAGG(
-                    JSON_OBJECT(
-                        'id', prog.id,
-                        'title', prog.title,
-                        'thumbnail', (SELECT pathname FROM files WHERE id = prog.thumbnail_id),
-                        'thumbnails', (SELECT JSON_ARRAYAGG(pathname) FROM files WHERE original_file_id = prog.thumbnail_id),
-                        'intro', (SELECT pathname FROM files WHERE id = prog.intro_id),
-                        'num_lectures', (SELECT COUNT(*) FROM lectures WHERE program_id = prog.id),
-                        'exercise', (SELECT title FROM exercises e INNER JOIN program_exercises pe ON e.id = pe.exercise_id WHERE pe.program_id=prog.id),
-                        'type', prog.type
-                    )
-                ) AS related_programs,
-                DATE_FORMAT(prod.release_at, '%Y-%m-%d %H:%i:%s') AS release_at,
-                CASE
-                    WHEN prod.release_at > NOW() THEN 'comming'
-                    ELSE 'released'
-                END AS status,
-                prod.is_hidden,
-                e.title AS exercises
-            FROM
-                products prod
-            INNER JOIN
-                    brands b
-                ON b.id = prod.brand_id
-            LEFT OUTER JOIN
-                    product_images pi
-                ON pi.product_id = prod.id
-            LEFT OUTER JOIN
-                    files f
-                ON f.id = pi.file_id
-            LEFT OUTER JOIN
-                    program_products pp
-                ON prod.id = pp.product_id
-            LEFT OUTER JOIN
-                    programs prog
-                ON prog.id = pp.program_id
-            LEFT OUTER JOIN
-                    product_exercises pe
-                    ON prod.id = pe.product_id
-            LEFT OUTER JOIN
-                    exercises e
-                        ON pe.exercise_id = e.id
-            WHERE prod.type='{parameters[0]}'
+    SELECT
+       prod.id,
+       prod.type,
+       prod.code,
+       prod.title as title,
+       prod.description,
+       b.title as brandTitle,
+       prod.original_price as original_price,
+       prod.price as price,
+       CASE
+           WHEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0)) <= 0 THEN 0
+           WHEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0)) > 0 THEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0))
+       END AS stock,
+       (
+           SELECT
+               f2.pathname
+           FROM
+                files f2
+            INNER JOIN product_images pi2
+           ON pi2.file_id = f2.id
+            WHERE pi2.type='thumbnail' AND pi2.product_id=prod.id
+        ) AS thumbnail,
+       (
+           SELECT JSON_ARRAYAGG(pathname) FROM files
+               INNER JOIN product_images
+                    ON product_images.file_id = files.original_file_id
+           WHERE product_images.product_id = prod.id AND product_images.type = 'thumbnail'
+       ) AS thumbnails,
+       (
+           SELECT JSON_ARRAYAGG(pathname) FROM files
+               INNER JOIN product_images
+                   ON product_images.file_id = files.id
+           WHERE product_images.type = 'detail' AND product_images.product_id = prod.id
+       ) AS details,
+       (
+           SELECT JSON_ARRAYAGG(pathname) FROM files
+               INNER JOIN product_images
+                    ON product_images.file_id = files.original_file_id
+           WHERE product_images.type = 'detail' AND product_images.product_id = prod.id
+       ) AS detail_thumbnails,
+       JSON_ARRAYAGG(
+            JSON_OBJECT(
+                'id', prog.id,
+                'title', prog.title,
+                'thumbnail', (SELECT pathname FROM files WHERE id = prog.thumbnail_id),
+                'thumbnails', (SELECT JSON_ARRAYAGG(pathname) FROM files WHERE original_file_id = prog.thumbnail_id),
+                'intro', (SELECT pathname FROM files WHERE id = prog.intro_id),
+                'num_lectures', (SELECT COUNT(*) FROM lectures WHERE program_id = prog.id),
+                'exercise', (SELECT title FROM exercises e INNER JOIN program_exercises pe ON e.id = pe.exercise_id WHERE pe.program_id=prog.id),
+                'type', prog.type
+            )
+        ) AS related_programs,
+        DATE_FORMAT(prod.release_at, '%Y-%m-%d %H:%i:%s') AS release_at,
+        CASE
+            WHEN prod.release_at > NOW() THEN 'comming'
+            ELSE 'released'
+        END AS status,
+        prod.is_hidden,
+        e.title AS exercises
+    FROM
+        products prod
+    INNER JOIN
+            brands b
+        ON b.id = prod.brand_id
+    LEFT OUTER JOIN
+            product_images pi
+        ON pi.product_id = prod.id
+    LEFT OUTER JOIN
+            files f
+        ON f.id = pi.file_id
+    LEFT OUTER JOIN
+            program_products pp
+        ON prod.id = pp.product_id
+    LEFT OUTER JOIN
+            programs prog
+        ON prog.id = pp.program_id
+    LEFT OUTER JOIN
+            product_exercises pe
+            ON prod.id = pe.product_id
+    LEFT OUTER JOIN
+            exercises e
+                ON pe.exercise_id = e.id
+                WHERE prod.type='{parameters[0]}'
 --                 AND prod.is_hidden = 1
-            GROUP BY prod.id, prog.id"""
+                GROUP BY prod.id, prog.id"""
     cursor.execute(sql)
     result = cursor.fetchall()
     connection.close()
@@ -220,28 +220,13 @@ def read_products():
     products_df = pd.DataFrame(result, columns=['id', 'type', 'code',
                                                 'title', 'description', 'brandTitle',
                                                 'original_price', 'price', 'stocks',
-                                                'thumbnail', 'thumbnails', 'details', 'detail_thumbnails',
-                                                'related_programs', 'release_at', 'status', 'is_hidden', 'exercises'])
+                                                'thumbnail', 'details', 'related_programs',
+                                                'release_at', 'status', 'is_hidden', 'exercises'])
     try:
-        products_df['details'] = products_df['details'].apply(lambda x: [] if x is None else json.loads(x))
-        # products_df['details'] = products_df['details'].apply(lambda x: json.loads(x))
-        # products_df['details'] = products_df['details'].apply(lambda x: [] if x.isna() is True else x)
+        products_df['details'] = products_df['details'].apply(lambda x: json.loads(x))
+        products_df['details'] = products_df['details'].apply(lambda x: [] if x[0] == "" else x)
     except:
-        result_dict = json.loads(products_df.to_json(orient='records'))[0]  # Array type으로 가고있음
-        return json.dumps(result_dict, ensure_ascii=False), 200
-    try:
-        products_df['detail_thumbnails'] = products_df['detail_thumbnails'].apply(lambda x: [] if x is None else json.loads(x))
-        # products_df['detail_thumbnails'] = products_df['detail_thumbnails'].apply(lambda x: json.loads(x))
-        # products_df['detail_thumbnails'] = products_df['detail_thumbnails'].apply(lambda x: [] if x.isna() is True else x)
-    except:
-        result_dict = json.loads(products_df.to_json(orient='records'))[0]  # Array type으로 가고있음
-        return json.dumps(result_dict, ensure_ascii=False), 200
-    try:
-        products_df['thumbnails'] = products_df['thumbnails'].apply(lambda x: [] if x is None else json.loads(x))
-        # products_df['thumbnails'] = products_df['thumbnails'].apply(lambda x: json.loads(x))
-        # products_df['thumbnails'] = products_df['thumbnails'].apply(lambda x: [] if x.isna() is True else x)
-    except:
-        result_dict = json.loads(products_df.to_json(orient='records'))[0]  # Array type으로 가고있음
+        result_dict = json.loads(products_df.to_json(orient='records'))  # Array type으로 가고있음
         return json.dumps(result_dict, ensure_ascii=False), 200
     try:
         products_df['related_programs'] = products_df['related_programs'].apply(lambda x: json.loads(x))
@@ -293,7 +278,7 @@ def read_a_product(product_id: int):
 
     cursor = connection.cursor()
     sql = f"""
-        SELECT
+        SELECT DISTINCT
            prod.id,
            prod.type,
            prod.code,
@@ -306,52 +291,31 @@ def read_a_product(product_id: int):
                WHEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0)) <= 0 THEN 0
                WHEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0)) > 0 THEN (IFNULL(prod.stocks, 0) - IFNULL((SELECT SUM(qty) FROM order_products WHERE product_id=prod.id), 0))
            END AS stock,
-           (
-               SELECT
+           (SELECT
                    f2.pathname
-               FROM
-                    files f2
-                INNER JOIN product_images pi2
-               ON pi2.file_id = f2.id
-                WHERE pi2.type='thumbnail' AND pi2.product_id=prod.id
-            ) AS thumbnail,
-           (
-               SELECT JSON_ARRAYAGG(pathname) FROM files
-                   INNER JOIN product_images
-                        ON product_images.file_id = files.original_file_id
-               WHERE product_images.product_id = prod.id AND product_images.type = 'thumbnail'
-           ) AS thumbnails,
-           (
-               SELECT JSON_ARRAYAGG(pathname) FROM files
-                   INNER JOIN product_images
-                       ON product_images.file_id = files.id
-               WHERE product_images.type = 'detail' AND product_images.product_id = prod.id
-           ) AS details,
-           (
-               SELECT JSON_ARRAYAGG(pathname) FROM files
-                   INNER JOIN product_images
-                        ON product_images.file_id = files.original_file_id
-               WHERE product_images.type = 'detail' AND product_images.product_id = prod.id
-           ) AS detail_thumbnails,
+           FROM
+                files f2
+            INNER JOIN product_images pi2
+           ON pi2.file_id = f2.id
+            WHERE pi2.type='thumbnail' AND pi2.product_id=prod.id) AS thumbnail,
+           JSON_ARRAYAGG(IFNULL(f.pathname, '')) AS details,
            JSON_ARRAYAGG(
                 JSON_OBJECT(
                     'id', prog.id,
                     'title', prog.title,
                     'thumbnail', (SELECT pathname FROM files WHERE id = prog.thumbnail_id),
-                    'thumbnails', (SELECT JSON_ARRAYAGG(pathname) FROM files WHERE original_file_id = prog.thumbnail_id),
-                    'intro', (SELECT pathname FROM files WHERE id = prog.intro_id),
+                    'intro', (SELECT pathname FROM files WHERE id = prog.intro_id),                    
                     'num_lectures', (SELECT COUNT(*) FROM lectures WHERE program_id = prog.id),
                     'exercise', (SELECT title FROM exercises e INNER JOIN program_exercises pe ON e.id = pe.exercise_id WHERE pe.program_id=prog.id),
                     'type', prog.type
-                )
-            ) AS related_programs,
+                )) AS related_programs,
             DATE_FORMAT(prod.release_at, '%Y-%m-%d %H:%i:%s') AS release_at,
             CASE
                 WHEN prod.release_at > NOW() THEN 'comming'
                 ELSE 'released'
             END AS status,
             prod.is_hidden,
-            e.title AS exercises
+            e.title AS exercises            
         FROM
             products prod
         INNER JOIN
@@ -374,10 +338,10 @@ def read_a_product(product_id: int):
                 ON prod.id = pe.product_id
         LEFT OUTER JOIN
                 exercises e
-                    ON pe.exercise_id = e.id      
+                    ON pe.exercise_id = e.id            
         WHERE prod.id = {product_id}
 --         AND prod.is_hidden = 1
-        GROUP BY prod.id, prog.id"""
+        GROUP BY prod.id"""
     cursor.execute(sql)
 
     result = cursor.fetchall()
@@ -389,27 +353,13 @@ def read_a_product(product_id: int):
     products_df = pd.DataFrame(result, columns=['id', 'type', 'code',
                                                 'title', 'description', 'brandTitle',
                                                 'original_price', 'price', 'stocks',
-                                                'thumbnail', 'thumbnails', 'details', 'detail_thumbnails',
-                                                'related_programs', 'release_at', 'status', 'is_hidden', 'exercises'])
+                                                'thumbnail', 'details', 'related_programs',
+                                                'release_at', 'status', 'is_hidden', 'exercises'])
     try:
-        products_df['details'] = products_df['details'].apply(lambda x: [] if x is None else json.loads(x))
-        # products_df['details'] = products_df['details'].apply(lambda x: json.loads(x))
-        # products_df['details'] = products_df['details'].apply(lambda x: [] if x.isna() is True else x)
+        products_df['details'] = products_df['details'].apply(lambda x: json.loads(x))
+        products_df['details'] = products_df['details'].apply(lambda x: [] if x[0] == "" else x)
     except:
-        result_dict = json.loads(products_df.to_json(orient='records'))[0]  # Array type으로 가고있음
-        return json.dumps(result_dict, ensure_ascii=False), 200
-    try:
-        products_df['detail_thumbnails'] = products_df['detail_thumbnails'].apply(lambda x: [] if x is None else json.loads(x))
-        # products_df['detail_thumbnails'] = products_df['detail_thumbnails'].apply(lambda x: json.loads(x))
-        # products_df['detail_thumbnails'] = products_df['detail_thumbnails'].apply(lambda x: [] if x.isna() is True else x)
-    except:
-        result_dict = json.loads(products_df.to_json(orient='records'))[0]  # Array type으로 가고있음
-        return json.dumps(result_dict, ensure_ascii=False), 200
-    try:
-        products_df['thumbnails'] = products_df['thumbnails'].apply(lambda x: [] if x is None else json.loads(x))
-        # products_df['thumbnails'] = products_df['thumbnails'].apply(lambda x: json.loads(x))
-        # products_df['thumbnails'] = products_df['thumbnails'].apply(lambda x: [] if x.isna() is True else x)
-    except:
+        # 썸네일만 없는 경우.
         result_dict = json.loads(products_df.to_json(orient='records'))[0]  # Array type으로 가고있음
         return json.dumps(result_dict, ensure_ascii=False), 200
     try:
