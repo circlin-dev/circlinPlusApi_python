1e0aa52cf40402a05898cbb8eb84615b56270c98
circlindev-caleb <caleb@circlin.co.kr>
2022-03-21 17:59:44 +0900

    Fixing bodylab: Adding Atflee OCR.

--- api/bodylab.py
+++ api/bodylab.py
@@ -1112,10 +1112,6 @@ def atflee_image():
     secure_file = secure_filename(atflee_image.filename)
     atflee_image.save(secure_file)
 
-    ocr_result = analyze_atflee_images(secure_file)
-    os.remove(secure_file)
-    status_code = ocr_result['status_code']
-    del ocr_result['status_code']
 
     now = datetime.now().strftime('%Y%m%d%H%M%S')
     # S3 업로드 - 바디랩 이미지 1: 신체 사진(눈바디)
@@ -1129,6 +1125,11 @@ def atflee_image():
         return json.dumps(result, ensure_ascii=False), 400
     atflee_input_image_dict = atflee_analysis['input_image_dict']
     resized_atflee_images_list = atflee_analysis['resized_images_list']
+
+    ocr_result = analyze_atflee_images(secure_file)
+    os.remove(secure_file)
+    status_code = ocr_result['status_code']
+    del ocr_result['status_code']
 
     if ocr_result['result'] is False:
         connection.close()
