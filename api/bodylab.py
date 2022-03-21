d34cd5655a4fc1a8793425eaf154b2cdbcc5791d
circlindev-caleb <caleb@circlin.co.kr>
2022-03-21 20:04:06 +0900

    Fixing bodylab: Adding Atflee OCR.

--- api/bodylab.py
+++ api/bodylab.py
@@ -1109,22 +1109,24 @@ def atflee_image():
     # bodylab_analyze_atflees = Table('bodylab_analyze_atflees')
     # user_questions = Table('user_questions')
     # files = Table('files')
+
     # S3 업로드 - 바디랩 이미지 1: 신체 사진(눈바디)
-    now = datetime.now().strftime('%Y%m%d%H%M%S')
-    atflee_analysis = validate_and_save_to_s3('atflee', atflee_image, user_id, now)
-    if atflee_analysis['result'] is False:
-        result = {
-            'result': False,
-            # 'message': atflee_analysis['error']
-            'message': atflee_analysis,
-            'filename': atflee_image.filename,
-            # 'ocr_result': ocr_result,
-        }
-        return json.dumps(result, ensure_ascii=False), 400
+    # now = datetime.now().strftime('%Y%m%d%H%M%S')
+    # atflee_analysis = validate_and_save_to_s3('atflee', atflee_image, user_id, now)
+    # if atflee_analysis['result'] is False:
+    #     result = {
+    #         'result': False,
+    #         # 'message': atflee_analysis['error']
+    #         'message': atflee_analysis,
+    #         'filename': atflee_image.filename,
+    #         # 'ocr_result': ocr_result,
+    #     }
+    #     return json.dumps(result, ensure_ascii=False), 400
 
     secure_file = secure_filename(atflee_image.filename)
     atflee_image.save(secure_file)
-    ocr_result = analyze_atflee_images(f'/home/ubuntu/circlinMembersApi_flask/{atflee_image.filename}')
+    ocr_result = analyze_atflee_images(atflee_image.filename)
+    # ocr_result = analyze_atflee_images(atflee_analysis['input_image_dict']['pathname'])
     status_code = ocr_result['status_code']
     del ocr_result['status_code']
 
@@ -1132,11 +1134,10 @@ def atflee_image():
         connection.close()
         return json.dumps(ocr_result, ensure_ascii=False), status_code
 
-
-    atflee_input_image_dict = atflee_analysis['input_image_dict']
-    resized_atflee_images_list = atflee_analysis['resized_images_list']
-    ocr_result['input_image_data'] = atflee_input_image_dict
-    ocr_result['resized_image_data'] = resized_atflee_images_list
+    # atflee_input_image_dict = atflee_analysis['input_image_dict']
+    # resized_atflee_images_list = atflee_analysis['resized_images_list']
+    # ocr_result['input_image_data'] = atflee_input_image_dict
+    # ocr_result['resized_image_data'] = resized_atflee_images_list
 
     return json.dumps(ocr_result, ensure_ascii=False), 200
 
